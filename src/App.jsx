1341b8c05d31c77942514895106f1fe5dc2523c1
nodirchik6903 <nu2913634@gmail.com>
2023-11-23 21:36:10 +0500

    Web telegram bot start

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {getData} from "./constants/db"
+import Card from './constants/card/card';
+
+const courses = getData()
+
+const App = () => {
+  return (
+    <>
+      <h1 className='heading'>Nodirbek Kurslari</h1>
+      {/* {Card} */}
+      <div className="cards__container">
+        {courses.map(course => (
+          <>
+            <Card key={course.id} course={course} />
+          </>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
